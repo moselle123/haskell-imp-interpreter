8b1ccb78e6eda1f00217c011f9887d2b81bbaf61
moselle123 <m.pattison.christie@gmail.com>
2025-02-28 18:50:45 +0000

    complete basic functions

--- Basic.hs
+++ Basic.hs
@@ -1,33 +1,29 @@
- 
-------------------------------------------------------------------- 
--- HASKELL BASIC LIST AND STRING PROCESSING FUNCTIONS           
--- Roy L. Crole and Paula Severi 2025                                      
-------------------------------------------------------------------- 
+
+-------------------------------------------------------------------
+-- HASKELL BASIC LIST AND STRING PROCESSING FUNCTIONS
+-- Roy L. Crole and Paula Severi 2025
+-------------------------------------------------------------------
 
 module Basic where
 
 import AST
 
--- code mem 
+-- code mem
 
-{-- 
 mem :: Eq a => a -> [a] -> Bool
-mem x []     = ???
-mem x (h:t)  = ???
---}
+mem x []     = False
+mem x (h:t)  = x == h || mem x t
 
 -- code lookUp
 
-{-- 
 lookUp :: Eq a => [(a,b)] -> a -> Either Error b
-lookUp [] v             = ???
-lookUp ((x,y):pairs) v  = ???
---}
+lookUp [] v             = Left UninitializedVar
+lookUp ((x,y):pairs) v  =
+        if x == v then Right y else lookUp pairs v
 
 -- code update
 
-{--
 update :: Eq a => [(a, b)] -> a -> b -> [(a, b)]
-update [] v z = ???
-update ((v,z):pairs) v' z'  = ???
---}
+update [] v z = [(v, z)]
+update ((v,z):pairs) v' z'  =
+        if v == v' then (v, z'):pairs else (v, z):update pairs v' z'
